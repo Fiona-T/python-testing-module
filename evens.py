f79a01de1c9ab445aa9c847dbf51050b25e6c0ef
Fiona-T <fitracey@gmail.com>
2021-10-07 10:40:32 +0000

    add functionality to even_number_of_evens through building tests, all tests pass

--- evens.py
+++ evens.py
@@ -6,7 +6,25 @@ def even_number_of_evens(numbers):
     if the number of even numbers is odd - return False
     if the numner of even numbers is even - return True
     """
-    return None
+    if isinstance(numbers, list):
+        if numbers == []:
+            return False
+        else:
+            # if list not empty, then need to check number of even no.s
+            evens = 0
+        # if number in list is even, add it to evens
+        for n in numbers:
+            if n % 2 == 0:
+                evens += 1
+        # using evens as Truthy/Falsy - true if greater than 0, false if 0
+        if evens:
+            # this returns true if evens is an even number, false if not
+            return evens % 2 == 0
+        else:
+            return False
+    else:
+        raise TypeError("A list was not passed into the function")
 
 
-print(even_number_of_evens(5))
+if __name__ == "__main__":
+    print(even_number_of_evens(5))
--- test_evens.py
+++ test_evens.py
@@ -1,9 +1,29 @@
+# file must start with the word test
 import unittest
+# import function to test
 from evens import even_number_of_evens
 
 
+# test case, extending from unittest
 class TestEvens(unittest.TestCase):
-    pass
+    def test_throws_error_if_value_passed_in_is_not_list(self):
+        # should raise TypeError calling function with value of 4
+        self.assertRaises(TypeError, even_number_of_evens, 4)
+
+    def test_values_in_list(self):
+        # should return false if function called with empty list
+        self.assertEqual(even_number_of_evens([]), False)
+        # should return True if fn called with even list of even numbers
+        self.assertEqual(even_number_of_evens([2, 4]), True)
+        # should return false if fn called with only one even number
+        self.assertEqual(even_number_of_evens([2]), False)
+        # should return false if fn called with odd number of odd numbers
+        self.assertEqual(even_number_of_evens([1, 3, 5]), False)
+
+    # def test_function_returns_True(self):
+    #     # testing if calling function with empty list returns True
+    #     self.assertTrue(even_number_of_evens([]))
 
 
-unittest.main()
+if __name__ == "__main__":
+    unittest.main()
